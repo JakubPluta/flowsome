78f2413bbe139e5facf297ef0b369ee03ccb6267
jp <plutakuba@gmail.com>
2024-03-24 16:00:04 +0100

    feat: change client to easier approach

--- flowsome/client.py
+++ flowsome/client.py
@@ -7,7 +7,6 @@ class Backend:
     def __init__(self, flow):
         self._flow = flow
         
-    
     def execute(self, *args, **kwargs):
         raise NotImplementedError
     
@@ -23,42 +22,39 @@ class PolarsBackend(Backend):
         data.sink_parquet(*args, **kwargs) 
     
  
+
 class Client:
+    
     def __init__(self, backend: Backend, config=None):
         self._backend = backend
         self._config = config
-        self._pipeline = []
+        self._pipe = None
         
     def read(self, file_path: str, schema: Dict | None = None, *args, **kwargs):
-        func = getattr(pl, "scan_csv")
-        self._pipeline.append(partial(func, file_path, schema=schema, *args, **kwargs))
-        return self
+        self._pipe = pl.scan_csv(file_path, schema=schema, *args, **kwargs)
+        return self 
+    
+    @property
+    def _empty_flow(self) -> bool:
+        return self._pipe is None
     
     def filterby(self, *predicates, **kw_predicates):
-        if not self._pipeline:
+        if self._empty_flow:
             raise ValueError("No read operation has been performed yet.")
-        
-        filter_func = getattr(pl.LazyFrame, "filter")
-        self._pipeline.append(partial(filter_func, *predicates, **kw_predicates))
+        self._pipe = self._pipe.filter(*predicates, **kw_predicates)
         return self
-        
-
+    
     def limit(self, n: int):
-        if not self._pipeline:
+        if self._empty_flow:
             raise ValueError("No read operation has been performed yet.")
-        limit_func = getattr(pl.LazyFrame, "limit")
-        self._pipeline.append(partial(limit_func, n=n))
+        self._pipe = self._pipe.limit(n)
         return self
     
     def to_parquet(self, file_path: str, *args, **kwargs):
-        backend = self._backend(self._pipeline)
-        return backend.execute(file_path, *args, **kwargs)
-
-
-
-
-
-
+        if self._empty_flow:
+            raise ValueError("No read operation has been performed yet.")
+        self._pipe.sink_parquet(file_path, *args, **kwargs)
+        return self
         
         
     
--- flowsome/main.py
+++ flowsome/main.py
@@ -7,7 +7,7 @@ client = Client(backend=PolarsBackend, config=Config())
 
 file_path = r'tests\data\sample.csv'
 
-client.read(file_path, schema=None).filterby(Country = "Malta").limit(n=100).to_parquet(file_path="data.parquet")
+client.read(file_path, schema=None).filterby(Country = "Sri Lanka").limit(n=2).to_parquet(file_path="data.parquet")
 
-pq = pl.scan_parquet(r"data.parquet")
-print(pq.collect())
+
+print(client._pipe.collect())
