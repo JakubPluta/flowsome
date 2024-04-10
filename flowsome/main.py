6b641fbeff7beb23cbfa9e1f1b55994002e5e407
jp <plutakuba@gmail.com>
2024-04-10 21:21:25 +0200

    other: pipeline v2 implementation - support for 2 sources with merge task

--- flowsome/main.py
+++ flowsome/main.py
@@ -1,25 +1,32 @@
-from flowsome.pipeline import Pipeline, PipelineOrchestrator
+from flowsome.pipeline import Pipeline, PipelineRunner
 from flowsome.tasks import *
 
 import polars as pl 
 
 if __name__ == "__main__":
     
-    po = PipelineOrchestrator()
-    po.add_task("Root1", TaskType.read, fmt="csv", source=r"tests\data\sample.csv")
-    po.add_task("Task1", TaskType.transform, "Root1", "filter", Country="Cyprus")
+    pipe = Pipeline()
+    
+    task1 = ReadTask("read1", "csv", source=r"tests/data/sample.csv")
+    task2 = TransformTask("transform1", "filter", Country="Cyprus")
+    task3 = TransformTask("transform2",  "limit", n=33)
+    task4 = WriteTask("write", "csv" , path=r"tests\data\output.csv")
+
+    read2 = ReadTask("read2", "csv", source=r"tests/data/sample.csv")
+    task5 = TransformTask("transform3", "limit", n=63)
+
+    merge_task = MergeTask("merge", "join", on="Email", how="right")
+
+    pipe.add_node(task1)
+    pipe.add_downstream(task1, task2)
+    pipe.add_downstream(task2, task3)
+    pipe.add_node(read2)
+    pipe.add_downstream(read2, task5)
+    pipe.add_downstream(task3, merge_task)
+    pipe.add_downstream(task5, merge_task)
+    pipe.add_downstream(merge_task, task4)
 
 
-
-    po.add_task("Root2", TaskType.read, fmt="csv", source=r"tests\data\sample.csv")
-    po.add_task("Task2", TaskType.transform, "Root2", "limit", n=33)
-
-
-    po.add_task("Merge", TaskType.merge, ["Task1", "Task2"], "join", on="Email", how="right")
-    po.add_task("Task3", TaskType.transform, "Merge", "limit", n=23)
-    po.add_task("Writer", TaskType.write, "Task3", "csv", path=r"tests\data\output.csv", maintain_order=False)
-
-
-    po.execute()
-
-
+    runner = PipelineRunner(pipeline=pipe)
+    runner.run()
+    
--- flowsome/pipeline.py
+++ flowsome/pipeline.py
@@ -1,153 +1,195 @@
-from __future__ import annotations
-import enum
-from typing import List, Optional
-
-from collections import OrderedDict, deque
-import enum
-from functools import cached_property
-from typing import Any, List, Sequence
-import polars as pl
-from flowsome.utils import get_scan_method, get_sink_method
+from copy import copy
+from flowsome.tasks import Task, TaskType, ReadTask, MergeTask, WriteTask, TransformTask
+from typing import List, Any, Dict, Tuple
 from flowsome.log import get_logger
-from flowsome.tasks import Task, TaskType, ReadTask, WriteTask, MergeTask, TransformTask
 
 
 log = get_logger(__name__)
 
 
 class Pipeline:
+    
     def __init__(self) -> None:
-        self._tasks: List[Task] = []
+        self._nodes: List[Task] = []
         
+    def __len__(self):
+        return len(self._nodes)
+    
     def __repr__(self) -> str:
-        return f"Pipeline(tasks={self._tasks})"
+        return f"Pipeline(nodes={self._nodes})"
     
-    def get_task(self, task_id: str) -> Task | None:
+    def __iter__(self):
+        for node in self._nodes:
+            yield from self.iter_from_node(node)
+            
+    def __contains__(self, task: Task | str) -> bool:
+        return self.contains_node(task)
+    
+    @property
+    def nodes(self) -> List[Task]:
+        return list(self._nodes)
+            
+    def contains_node(self, task: Task | str) -> bool:
+        if isinstance(task, str):
+            return self.find_node(task) is not None
+        return task in self._nodes
+    
+    
+    def iter_from_node(self, node: Task):
+        while node.successor:
+            yield node 
+            node = node.successor
+        yield node
+            
+    def iter_tasks(self):
+        for node in self._nodes:
+            yield from self.iter_from_node(node)
+
+    def list_pipelines(self):
+        return [list(self.iter_from_node(node)) for node in self._nodes]
+    
+    def list_tasks(self):
+        tasks = copy(self._nodes)
+        for node in self._nodes:
+            while node.successor:
+                node = node.successor
+                tasks.append(node)
+        return list(set(tasks))
+    
+    def find_task(self, task_id: str) -> Task:
+        for node in self._nodes:
+            for task in self.iter_from_node(node):
+                if task.task_id == task_id:
+                    return task 
+        raise LookupError("couldn't find task in nodes, and all downstreams")
+    
+    def find_node(self, task_id: str) -> Task:
+        for node in self._nodes:
+            if node.task_id == task_id:
+                return node
+        raise LookupError("couldn't find task in nodes")
+        
+    
+    def add_node(self, task: Task) -> None:
+        if task in self._nodes:
+            raise ValueError("%s already added", task)
+        self._nodes.append(task)
+        
+    def remove_node(self, task_id: str) -> None:
+        found = False
+        for node in self._nodes:
+            if node.task_id == task_id:
+                found = True
+                self._nodes.remove(node)
+        if found is False:
+            raise ValueError("couldn't delete task %s - doesn't exists in nodes list")
+            
+    
+    def add_downstream(self, task: Task, downstream_task: Task):
+        if task not in self.list_tasks():
+            raise ValueError("parent task not exists in ")
+        
+        for dtask in self.iter_from_node(task):
+            if dtask.task_id == downstream_task.task_id:
+                raise ValueError("downstream %s already in added to task %s", downstream_task, task)
+        task.add_successor(downstream_task)
+
+
+    def add_upstream(self, task: Task, upstream_task: Task):
+        if not self.contains_node(task):
+            raise ValueError("Parent task not exists in nodes list")
+        
+        for dtask in self.iter_from_node(task):
+            if dtask.task_id == upstream_task.task_id:
+                raise ValueError("upstream task %s alread a downstream to task %s", upstream_task, task)
+    
+        for node in self._nodes:
+            if node.task_id == task.task_id:
+                self._nodes.pop(task)
+                break 
+        
+        upstream_task.add_successor(task)
+        self._nodes.append(upstream_task)
+
+
+
+class PipelineRunner:
+    
+    def __init__(self, pipeline: Pipeline) -> None:
+        self.pipeline = pipeline
+        self.execution_plans = self.pipeline.list_pipelines()
+    
+    def is_simple_pipeline(self) -> bool:
+        return len(self.execution_plans) == 1
+    
+    def _execution_plans_are_equal(self, list_of_lists: List[List[Task]]) -> bool:
+        return all(list_of_lists[i] == list_of_lists[i + 1] for i in range(len(list_of_lists) - 1))
+    
+    @staticmethod
+    def _split_list_by_value(input_list: List[Any], split_value: List[Any]) -> Tuple[List[Any], List[Any]]:
+        list1, list2 = None, None
         try:
-            return next(task for task in self._tasks if task.task_id == task_id)
-        except StopIteration:
+            split_index = input_list.index(split_value)
+            list1 = input_list[:split_index]
+            list2 = input_list[split_index:]
+        except ValueError:
+            log.info("Couldn't find split value in list")
+        return list1, list2
+    
+    def _split_execution_plans(self, execution_plans: List[List[Task]], common_task: Task) -> Tuple[List[List[Task]], List[List[Task]]]:
+        plans_before_merge, plans_after_merge = [], []
+        for plan in execution_plans:
+            before_merge, after_merge = self._split_list_by_value(plan, common_task)
+            plans_before_merge.append(before_merge)
+            plans_after_merge.append(after_merge)
+        return plans_before_merge, plans_after_merge
+
+    def _find_first_common_task(self) -> Task | None:
+        if not self.execution_plans:
             return None
-  
-    
-    def is_task_in_pipeline(self, task_id: str) -> bool:
-        return self.get_task(task_id) is not None
-    
-    def is_succesor_of_task(self, task: Task, successor: Task) -> bool:
-        return successor == task.successor
-    
-    
-    def add_task(self, task: Task):
-        if self.is_task_in_pipeline(task.task_id):
-            raise Exception(f"Task: {task.task_id} already in pipeline")
-        if task.is_merge_task() and self.is_merge_task_in_pipeline():
-            raise Exception("Merge task already in pipeline. Only one merge task is allowed")
-        self._tasks.append(task)
-    
-    def add_successor(self, task: Task | str, successor: Task):
-        if self.is_succesor_of_task(task, successor):
-            raise Exception(f"Task: {successor.task_id} already in successors list")
         
-        if task is successor:
-            raise Exception(f"Task: {successor.task_id} cannot be successor of itself")
-        task.add_successor(successor)
-        self._tasks.append(successor)
-        
-        
-    def iter_successors(self, task: Task):
-        while task.successor:
-            task = task.successor
-            yield task
-                
-    def find_root_tasks(self) -> List[ReadTask]:
-        return [task for task in self._tasks if task.is_root()]
+        # Get the tasks from the first plan
+        first_plan_tasks = self.execution_plans[0]
+
+        # Iterate through the tasks in the first plan
+        for task in first_plan_tasks:
+            # Check if the task is present in all other plans
+            if all(task in plan for plan in self.execution_plans[1:]):
+                return task
+        return None
+     
     
-
-    def is_merge_task_in_pipeline(self) -> bool:
-        for task in self._tasks:
-            if task._type == TaskType.merge:
-                return True
-        return False
-
-
-
-class PipelineOrchestrator:
-    task_registry = {
-        TaskType.read: ReadTask,
-        TaskType.transform: TransformTask,
-        TaskType.write: WriteTask,
-        TaskType.merge: MergeTask
-    }
-    def __init__(self) -> None:
-        self.pipeline = Pipeline()
+    def run(self):
+        if len(self.pipeline) > 2:
+            raise ValueError("Pipeline with more than 2 nodes is not supported")
         
-    
-    def _create_task(self, task_name: str, task_type: TaskType, *args, **params) -> Task:
-        try:
-            return self.task_registry[task_type](task_name, *args, **params)
-        except (KeyError, AttributeError) as e:
-            log.error("Task type %s not found", task_type)
-            raise AttributeError from e
-
-        
-    def add_task(self, task_name: str, task_type: TaskType, predecessor_name: str | List = None, *args, **params) -> Task:
-        predecessor_tasks = None
-        if predecessor_name and isinstance(predecessor_name, list):
-            predecessor_tasks = [self.pipeline.get_task(predecessor_name) for predecessor_name in predecessor_name]
-            if not all(isinstance(predecessor_task, Task) for predecessor_task in predecessor_tasks):
-                raise ValueError("Predecessor task for merge must be a list of tasks")
-        elif predecessor_name and isinstance(predecessor_name, str):
-            predecessor_tasks = [self.pipeline.get_task(predecessor_name)]
-            
-        task = self._create_task(task_name, task_type, *args, **params)
-        
-        if predecessor_tasks:
-            for predecessor_task in predecessor_tasks:
-                predecessor_task.add_successor(task)
-        self.pipeline.add_task(task)
-        return task
-
-    def _execute_single_pipeline(self, read_task: ReadTask):
-        task: ReadTask = read_task
-        df: pl.LazyFrame = task.execute()
-        while task.successor:
-            if task.is_merge_task():
-                break
-            task = task.successor
-            df = task.execute(df)
-            return df
-    
-    
-    def _execute_from_merge_task(self, merge_task: MergeTask, df: pl.LazyFrame, other_df: pl.LazyFrame):
-        results = merge_task.execute(df, other_df)
-        while merge_task.successor:
-            merge_task = merge_task.successor
-            results = merge_task.execute(results)
-        return results
-
-    def _find_merge_task(self) -> MergeTask:
-        return [task for task in self.pipeline._tasks if task.is_merge_task()][0]
-    
-    
-    def execute(self):    
-        root_tasks = self.pipeline.find_root_tasks()
-        if len(root_tasks) == 0:
-            raise Exception("No root tasks found")
-        if len(root_tasks) > 2:
-            raise Exception("More than two root tasks found. Only one or twoo root tasks are supported")
-        
-        if len(root_tasks) == 1:
-            self._execute_single_pipeline(root_tasks[0])
+        if self.is_simple_pipeline():
+            execution_plan = self.execution_plans[0]
+            reader = execution_plan.pop(0)
+            df = reader.execute()
+            for task in execution_plan:
+                df = task.execute(df)
         else:
-            dfs = []
-            merge_task = self._find_merge_task()
-            if merge_task is None:
-                raise Exception("No merge task found in pipeline")
-            for root_task in root_tasks:
-                df = self._execute_single_pipeline(root_task)
-                dfs.append(df)
-            self._execute_from_merge_task(merge_task, *dfs)
+            first_common_task = self._find_first_common_task()
+            if first_common_task is None:
+                raise ValueError("No common task found")
+            plans_before_merge, plans_after_merge = self._split_execution_plans(self.execution_plans, first_common_task)
             
- 
-        
-        
-
+            if not self._execution_plans_are_equal(plans_after_merge):
+                raise ValueError("Execution plans after merge are not equal")
+            plan_after_merge = plans_after_merge[0]
+            
+            dfs = []
+            for pipe in plans_before_merge:
+                reader = pipe.pop(0)
+                df = reader.execute()
+                for task in pipe:
+                    df = task.execute(df)
+                dfs.append(df)
+            merge_task: MergeTask = plan_after_merge.pop(0)
+            if not merge_task.is_merge_task():
+                raise ValueError("first task after fan-in should be a merge task")
+            df = merge_task.execute(*dfs)
+            for task in plan_after_merge:
+                df = task.execute(df)
+            return df
